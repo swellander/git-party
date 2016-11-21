7a12d3798ea5e416fd00f70132395a047b338408
Gabriel Lebec <glebec@gmail.com>
2016-11-21 03:05:11 -0500

    feat(model-spec): model specs can now be run in any order

--- test/articles_model.test.js
+++ test/articles_model.test.js
@@ -13,190 +13,241 @@ var db = require('../models/database');
  *
  */
 
-describe('Articles', function () {
+describe('The `Article` model', function () {
 
   /**
-   * First we clear the database and recreate the tables before beginning each run
+   * First we clear the database and recreate the tables before beginning a run
    */
   before(function () {
     return db.sync({force: true});
   });
 
+  /**
+   * Next, we create an (un-saved!) article instance before every spec
+   */
   var fullText = 'The South African cliff swallow (Petrochelidon spilodera), also known as the South African swallow, is a species of bird in the Hirundinidae family.';
 
-  /**
-   * Your model should have two fields (both required): `title` and `content`.
-   *
-   * http://docs.sequelizejs.com/en/v3/docs/models-definition/#validations
-   */
-  it('has title and content fields', function () {
-
-    return Article.create({
+  var article;
+  beforeEach(function(){
+    article = Article.build({
       title: 'Migratory Birds',
       content: fullText
-    })
-    .then(function (savedArticle) {
-      expect(savedArticle.title).to.equal('Migratory Birds');
-      expect(savedArticle.content).to.equal(fullText);
     });
-
-  });
-
-  xit('requires content', function () {
-
-    var article = Article.build({
-      title: 'My Second Article'
-    });
-
-    return article.validate()
-    .then(function(result) {
-      expect(result).to.be.an.instanceOf(Error);
-      expect(result.message).to.contain('content cannot be null');
-    });
-
-  });
-
-  xit('requires title (in a more strict way than for content)', function () {
-
-    var article = Article.build({
-      title: '',
-      content: 'Some more wonderful text'
-    });
-
-    return article.validate()
-    .then(function (result) {
-      expect(result).to.be.an.instanceOf(Error);
-      expect(result.message).to.contain('Validation error');
-    });
-
-  });
-
-  xit('can handle long content', function() {
-
-    var articleContent = 'WALL-E (stylized with an interpunct as WALL·E) is a 2008 American computer-animated science-fiction comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. Directed by Andrew Stanton, the story follows a robot named WALL-E, who is designed to clean up an abandoned, waste-covered Earth far in the future. He falls in love with another robot named EVE, who also has a programmed task, and follows her into outer space on an adventure that changes the destiny of both his kind and humanity. Both robots exhibit an appearance of free will and emotions similar to humans, which develop further as the film progresses.';
-
-    return Article.create({
-      title: 'WALL-E',
-      content: articleContent
-    })
-    .then(function(result) {
-      expect(result).to.be.an('object');
-      expect(result.title).to.equal('WALL-E');
-      expect(result.content).to.equal(articleContent);
-    });
-
-  });
-
-
-  /**
-   * Set up a virtual field (check out sequelize getter methods) called `snippet`
-   * that returns the first 23 characters of the content followed by "...".
-   *
-   * http://docs.sequelizejs.com/en/v3/docs/models-definition/#defining-as-part-of-the-model-options
-   */
-  xit('has a virtual 23-character snippet field appended with "..."', function () {
-
-    return Article.findOne({ where: { title: 'Migratory Birds' } })
-    .then(function (article) {
-      expect(article.content).to.equal(fullText);
-      expect(article.snippet).to.equal('The South African cliff...');
-    });
-
   });
 
   /**
-   * Set up an instance method (check out sequelize instanceMethods) called `truncate`
-   * that will shorten the article instance content to a passed-in length.
-   * This method does not save to the backend, it just modifies the mongoose
-   * object so the user can choose if and when to actually save.
-   *
-   * http://docs.sequelizejs.com/en/v3/docs/models-definition/#expansion-of-models
+   * Also, we empty the tables after each spec
    */
-  xit('has an instance method to truncate the content', function () {
-
-    return Article.findOne({ where: { title: 'Migratory Birds' } })
-    .then(function (article) {
-      expect(article.content).to.equal(fullText);
-      article.truncate(12);
-      expect(article.content).to.equal('The South Af');
-    });
-
+  afterEach(function () {
+    return Promise.all([
+      Article.truncate({ cascade: true }),
+      User.truncate({ cascade: true })
+    ]);
   });
 
-  xit('content truncation accepts any length', function () {
+  describe('attributes definition', function(){
 
-    return Article.findOne({ where: { title: 'Migratory Birds' } })
-    .then(function (article) {
-      var randLength = Math.ceil(Math.random() * 20);
-      expect(article.content).to.equal(fullText);
-      article.truncate(randLength);
-      expect(article.content).to.have.length(randLength);
+    /**
+     * Your model should have two fields (both required): `title` and `content`.
+     *
+     * http://docs.sequelizejs.com/en/v3/docs/models-definition/#validations
+     */
+    it('includes `title` and `content` fields', function () {
+
+      return article.save()
+      .then(function (savedArticle) {
+        expect(savedArticle.title).to.equal('Migratory Birds');
+        expect(savedArticle.content).to.equal(fullText);
+      });
+
     });
 
-  });
+    xit('requires `content`', function () {
 
-  xit('does not save the content once truncated', function() {
+      article.content = null;
 
-    return Article.findOne({ where: { title: 'Migratory Birds' } })
-    .then(function(article) {
-      expect(article.content).to.equal(fullText);
-      article.truncate(7);
-      expect(article.content).to.have.length(7);
-      return Article.findOne({ where: { title: 'Migratory Birds' } })
-    })
-    .then(function(article) {
-      expect(article.content).to.equal(fullText);
+      return article.validate()
+      .then(function(result) {
+        expect(result).to.be.an.instanceOf(Error);
+        expect(result.message).to.contain('content cannot be null');
+      });
+
     });
 
-  });
+    xit('requires `title` (in a more strict way than for `content`)', function () {
 
-  /**
-   * Set up a class method called findByTitle that's a convenience
-   * method to find a *single* document by its title.
-   *
-   * http://docs.sequelizejs.com/en/v3/docs/models-definition/#expansion-of-models
-   */
+      article.title = '';
 
-  xit('class method findByTitle finds one article by its title', function () {
+      return article.validate()
+      .then(function (result) {
+        expect(result).to.be.an.instanceOf(Error);
+        expect(result.message).to.contain('Validation error');
+      });
 
-    return Article.findByTitle('Migratory Birds')
-    .then(function (article) {
-      expect(article).not.to.be.an.instanceOf(Array);
-      expect(article.content).to.equal(fullText);
     });
 
-  });
+    xit('can handle long `content`', function() {
 
-  /**
-   * Add a `belongsTo` relationship between articles and users,
-   * but make sure the user is called `author` for each article.
-   *
-   * http://docs.sequelizejs.com/en/v3/docs/associations/#belongsto
-   */
+      var articleContent = 'WALL-E (stylized with an interpunct as WALL·E) is a 2008 American computer-animated science-fiction comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. Directed by Andrew Stanton, the story follows a robot named WALL-E, who is designed to clean up an abandoned, waste-covered Earth far in the future. He falls in love with another robot named EVE, who also has a programmed task, and follows her into outer space on an adventure that changes the destiny of both his kind and humanity. Both robots exhibit an appearance of free will and emotions similar to humans, which develop further as the film progresses.';
 
-  xit('belongs to a user, who is stored as the article\'s `author`', function() {
-
-    var user;
-    return User.create({ name: 'Alatar the Blue'})
-    .then(function(createdUser) {
-      user = createdUser;
       return Article.create({
+        title: 'WALL-E',
+        content: articleContent
+      })
+      .then(function(result) {
+        expect(result).to.be.an('object');
+        expect(result.title).to.equal('WALL-E');
+        expect(result.content).to.equal(articleContent);
+      });
+
+    });
+
+  });
+
+  describe('options definition', function(){
+
+    describe('`snippet` virtual field', function(){
+
+      /**
+       * Set up a virtual field (check out sequelize getter methods) called `snippet`
+       * that returns the first 23 characters of the content followed by "...".
+       *
+       * http://docs.sequelizejs.com/en/v3/docs/models-definition/#defining-as-part-of-the-model-options
+       */
+      xit('evaluates to the first 23 characters of the `content` appended with "..."', function () {
+
+        expect(article.snippet).to.equal('The South African cliff...');
+
+        article.content = 'At length did cross an Albatross / Thorough the fog it came';
+        expect(article.snippet).to.equal('At length did cross an ...');
+
+        article.content = 'The Albatross fell off, and sank / Like lead into the sea';
+        expect(article.snippet).to.equal('The Albatross fell off,...');
+
+      });
+
+      // This is mostly to avoid a corner case seen during `Model.update`.
+      xit('returns empty string for missing `content`', function(){
+
+        article.content = undefined;
+
+        expect(article.snippet).to.equal('');
+
+      });
+
+    });
+
+    describe('`truncate` instance method', function(){
+
+      /**
+       * Set up an instance method (check out sequelize instanceMethods) called `truncate`
+       * that will shorten (change!) the article instance content to a passed-in length.
+       * This method does not save to the backend, it just modifies the Sequelize
+       * object so the user can choose if and when to actually save.
+       *
+       * http://docs.sequelizejs.com/en/v3/docs/models-definition/#expansion-of-models
+       */
+      xit('truncates the `content`', function () {
+
+        expect(article.content).to.equal(fullText);
+
+        article.truncate(12);
+        expect(article.content).to.equal('The South Af');
+
+      });
+
+      xit('accepts any length', function () {
+
+        expect(article.content).to.equal(fullText);
+
+        var randLength = Math.ceil(Math.random() * 20);
+        article.truncate(randLength);
+        expect(article.content).to.have.length(randLength);
+
+      });
+
+      xit('does not save the instance once truncated', function() {
+
+        expect(article.content).to.equal(fullText);
+
+        article.truncate(7);
+        expect(article.content).to.have.length(7);
+
+        return Article.findAll()
+        .then(function(articles) {
+          expect(articles).to.have.length(0);
+        });
+
+      });
+
+    });
+
+    describe('`findByTitle` class method', function(){
+
+      /**
+       * Set up a class method called `findByTitle` that's a convenience
+       * method to find a *single* document by its title.
+       *
+       * http://docs.sequelizejs.com/en/v3/docs/models-definition/#expansion-of-models
+       */
+
+      beforeEach(function(){
+        var otherArticles = [1, 2, 3].map(function (num) {
+          return Article.create({
+            title: 'Article Number ' + num,
+            content: 'etc.'
+          });
+        });
+        return Promise.all([...otherArticles, article.save()]);
+      });
+
+      xit('finds one specific article by its `title`', function () {
+
+        return Article.findByTitle('Migratory Birds')
+        .then(function (foundArticle) {
+          expect(foundArticle).not.to.be.an.instanceOf(Array);
+          expect(foundArticle.content).to.equal(fullText);
+        });
+
+      });
+
+    });
+
+  });
+
+  describe('associations', function(){
+
+    /**
+     * Add a `belongsTo` relationship between articles and users,
+     * but make sure the user is aliased as `author` for each article.
+     *
+     * http://docs.sequelizejs.com/en/v3/docs/associations/#belongsto
+     */
+
+    xit("belongs to a user, who is stored as the article's `author`", function() {
+
+      var creatingUser = User.create({ name: 'Alatar the Blue'});
+      var creatingArticle = Article.create({
         title: 'Blue Wizards',
         content: 'They are two of the five Wizards (or Istari) sent by the Valar to Middle-earth to aid in the struggle against Sauron.'
       });
-    })
-    .then(function(article) {
-      return article.setAuthor(user);
-    })
-    .then(function() {
-      return Article.findOne({
-        where: { title: 'Blue Wizards' },
-        include: { model: User, as: 'author' }
+
+      return Promise.all([creatingUser, creatingArticle])
+      .then(function([createdUser, createdArticle]) {
+        // this method `setAuthor` method automatically exists if you set up the association correctly
+        return createdArticle.setAuthor(createdUser);
+      })
+      .then(function() {
+        return Article.findOne({
+          where: { title: 'Blue Wizards' },
+          include: { model: User, as: 'author' }
+        });
+      })
+      .then(function(foundArticle){
+        expect(foundArticle.author).to.exist; // eslint-disable-line no-unused-expressions
+        expect(foundArticle.author.name).to.equal('Alatar the Blue');
       });
-    })
-    .then(function(article){
-      expect(article.author).to.exist; // eslint-disable-line no-unused-expressions
-      expect(article.author.name).to.equal('Alatar the Blue');
+
     });
 
   });
@@ -208,61 +259,73 @@ describe('Articles', function () {
    * http://docs.sequelizejs.com/en/v3/docs/hooks/
    */
 
-  describe('has a version field', function() {
+  describe('`version` field', function() {
 
-    before(function() {
+    beforeEach(function() {
       return Article.create({
         title: 'Biological Immortality',
         content: 'Biological immortality refers to a stable or decreasing rate of mortality from senescence, thus decoupling it from chronological age.'
       });
     });
 
-    xit('that is originally 0', function() {
+    xit('is originally 0, even if not explicitly set', function() {
 
       return Article.findOne({where: {title: 'Biological Immortality'}})
-      .then(function(article) {
-        expect(article.version).to.equal(0);
+      .then(function(foundArticle) {
+        expect(foundArticle.version).to.equal(0);
       });
 
     });
 
-    xit('that increments by 1 every time you update the document', function() {
+    xit('increments by 1 every time the article is updated', function() {
 
       return Article.findOne({where: {title: 'Biological Immortality'}})
-      .then(function(article) {
-        article.content = 'Biological immortality is a lie!';
-        return article.save();
+      .then(function(foundArticle) {
+        expect(foundArticle.version).to.equal(0);
+        return foundArticle.update({
+          content: 'Biological immortality is a lie!'
+        });
       })
-      .then(function(article) {
-        expect(article.version).to.equal(1);
+      .then(function(updatedArticle) {
+        expect(updatedArticle.version).to.equal(1);
+        return updatedArticle.update({
+          content: 'Have you seen the 19th century painting of Keanu Reeves?'
+        });
+      })
+      .then(function(updatedArticle) {
+        expect(updatedArticle.version).to.equal(2);
       });
 
     });
 
   });
 
-  /** EXTRA CREDIT
-   * Your Article model should have a tag field that's an array, but when we
-   * access it, we should get one string: the tags joined by a comma and space
-   *
-   * Look at getters and setters:
-   * http://docs.sequelizejs.com/en/v3/docs/models-definition/#getters-setters
-   *
-   * To activate this spec, change `xit` to `it`
-   */
-  xit('has a `tags` field with a custom getter', function () {
+  describe('extra credit `tags` field', function(){
 
-    // tags should have a `defaultValue` that is an empty array.
-    expect(Article.attributes.tags.defaultValue).to.deep.equal([]);
+    /** EXTRA CREDIT
+     * Your Article model should have a tag field that's an array, but when we
+     * access it, we should get one string: the tags joined by a comma and space
+     *
+     * Look at getters and setters:
+     * http://docs.sequelizejs.com/en/v3/docs/models-definition/#getters-setters
+     *
+     * To activate this spec, change `xit` to `it`
+     */
+    xit('is a custom getter', function () {
 
-    // main functionality of tags
-    return Article.create({
-      title: 'Taggy',
-      content: 'So Taggy',
-      tags: ['tag1', 'tag2', 'tag3']
-    })
-    .then(function (article) {
-      expect(article.tags).to.equal('tag1, tag2, tag3');
+      // tags should have a `defaultValue` that is an empty array.
+      expect(Article.attributes.tags.defaultValue).to.deep.equal([]);
+
+      // main functionality of tags
+      return Article.create({
+        title: 'Taggy',
+        content: 'So Taggy',
+        tags: ['tag1', 'tag2', 'tag3']
+      })
+      .then(function (createdArticle) {
+        expect(createdArticle.tags).to.equal('tag1, tag2, tag3');
+      });
+
     });
 
   });
